4814489846f17a9b3ddceacb38af446c246a515a
DESKTOP-RNP8MQQ\OCR <1402327769@qq.com>
2017-11-27 17:25:47 +0800

    111完成了

--- test111/main/main.js
+++ test111/main/main.js
@@ -1,20 +1,33 @@
+
 module.exports = function main(number){
-	let result = "";
-	if(number>1){
-		for(let i=number;i>1;i--){
-            result += i +"bottles of beer on the wall, " + i +"bottles of beer.\n";
-                  "Take one down and pass it around," + number-1 + "bottles of beer on the wall.;\n"
-        }
-	}else if(number == 1){
-		result +="1 bottle of beer on the wall, 1 bottle of beer.\n"+
-                  "Take one down and pass it around, no more bottles of beer on the wall.\n";
-	}
-	else if(number == 0){
-		result += "No more bottles of beer on the wall, no more bottles of beer.\n"+
-				  "Go to the store and buy some more, 99 bottles of beer on the wall.\n";
-	}else{
-		return "please given the number bigger than 0";
-	}
-	console.log(result)
+	let result = (number>=0)?output(number):retErr();;
 	return result;
+}
+
+
+function output(number){
+	let result = "";
+	
+	while(number>=0){
+		if(number>2){
+	        result += number.toString() +" bottles of beer on the wall, " + number.toString() +" bottles of beer.\n"+
+	             	 "Take one down and pass it around, " + (number-1).toString() + " bottles of beer on the wall.\n";
+		}else if(number == 2){
+			result +="2 bottles of beer on the wall, 2 bottles of beer.\n" +
+           			 "Take one down and pass it around, 1 bottle of beer on the wall.\n";
+		}else if(number == 1){
+			result +="1 bottle of beer on the wall, 1 bottle of beer.\n"+
+	                  "Take one down and pass it around, no more bottles of beer on the wall.\n";
+		}else if(number == 0){
+			result += "No more bottles of beer on the wall, no more bottles of beer.\n"+
+					  "Go to the store and buy some more, 99 bottles of beer on the wall.\n";
+		}
+		number--;
+	}
+	return result;
+}
+
+
+function retErr(){
+	return "error";
 }
